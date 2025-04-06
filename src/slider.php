d6812697f565d3abfe1eba048c37bbfbf1194cb9
chutu22 <chutu220703@gmail.com>
2025-04-06 23:53:50 +0700

    FE khoa hoc

---
+++ src/slider.php
@@ -0,0 +1,19 @@
+<section class="admin-content">
+    <div class="admin-content-left">
+        <ul>
+            <li><a href="">Danh mục</a>
+                <ul>
+                    <li><a href="">Thêm Khóa học</a></li>
+                    <li><a href="">Danh sách Khóa học</a></li>
+                </ul>
+            </li>
+            <li><a href="">Loại sản phẩm</a>
+                <ul>
+                    <li><a href="">Thêm Lớp học</a></li>
+                    <li><a href="">Danh sách Lớp</a></li>
+                </ul>
+            </li>
+                </ul>
+            </li>
+        </ul>
+    </div>
